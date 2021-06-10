8544b4a6fa656bb319dd8f81c192d218913fc184
prateekshyap <prateekshyapriyadarshini@gmail.com>
2021-06-10 17:33:21 +0530

    10/06

--- Stack/CelebrityProblem.java
+++ Stack/CelebrityProblem.java
@@ -44,3 +44,48 @@ class Solution
         return -1;
     }
 }
+
+//efficient solution using stack
+
+class Solution
+{
+    int celebrity(int M[][], int n)
+    {
+        //push all the ids into the stack
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i = 0; i < n; ++i)
+            stack.push(i);
+
+        //pop till there is one id left
+        while (stack.size() > 1)
+        {
+            //pop tow ids
+            int a = stack.pop();
+            int b = stack.pop();
+
+            //if a knows b, a cannot be a celebrity, so push b
+            if (M[a][b] == 1)
+                stack.push(b);
+
+            //if a doesn't know b, b cannot be a celebrity, so push a
+            else
+                stack.push(a);
+        }
+
+        //the remaining id is the potential candidate
+        int candidate = stack.pop();
+
+        //if the candidate knows someone, return -1
+        for (int i = 0; i < n; ++i)
+            if (M[candidate][i] == 1)
+                return -1;
+
+        //if there is someone who doesn't know the candidate, return -1
+        for (int i = 0; i < n; ++i)
+            if (i != candidate && M[i][candidate] == 0)
+                return -1;
+
+        //if both loops are uninterrupted, return the candidate
+        return candidate;
+    }
+}
---
+++ Stack/LRUCache.java
@@ -0,0 +1,164 @@
+/**/
+
+//LinkedHashMap implementation
+class LRUCache {
+    static LinkedHashMap<Integer,Integer> map;
+    static int capacity;
+    static Iterator iterator;
+
+    public LRUCache(int capacity) {
+        this.map = new LinkedHashMap<Integer,Integer>(capacity);
+        this.capacity = capacity;
+    }
+    
+    public int get(int key) {
+        //get the value
+        int result = map.containsKey(key) ? map.get(key) : -1;
+        
+        //if key is present
+        if (result != -1)
+        {
+            //remove and reinsert it
+            map.remove(key);
+            map.put(key,result);
+        }
+        return result;
+    }
+    
+    public void put(int key, int value) {
+        //if the key is present
+        if (map.containsKey(key))
+        {
+            //remove and reinsert it
+            map.remove(key);
+            map.put(key,value);
+        }
+
+        //if key is not present and no overflow
+        else if (map.size() < capacity)
+        {
+            //add the key
+            map.put(key,value);
+        }
+
+        //all other cases
+        else
+        {
+            //remove the element at 0th index
+            iterator = map.entrySet().iterator();
+            if (iterator.hasNext())
+            {
+                int remove = (Integer)((Map.Entry)iterator.next()).getKey();
+                map.remove(remove);
+            }
+
+            //add the new key
+            map.put(key,value);
+        }
+    }
+}
+
+//scratch implementation
+class Node
+{
+    Node prev;
+    int key;
+    int value;
+    Node next;
+    Node(int k, int v)
+    {
+        key = k;
+        value = v;
+    }
+}
+
+class Cache
+{
+    private Node head, tail;
+    private Node[] hashTable;
+    private int capacity;
+    private int size;
+    
+    Cache(int capacity)
+    {
+        this.capacity = capacity;
+        size = 0;
+        //two dummy nodes to handle edge cases
+        head = new Node(-1,-1);
+        tail = new Node(-1,-1);
+        head.next = tail;
+        tail.prev = head;
+        hashTable = new Node[10001];
+    }
+    private void bubbleUp(int key)
+    {
+        hashTable[key].prev.next = hashTable[key].next;
+        hashTable[key].next.prev = hashTable[key].prev;
+        hashTable[key].prev = tail.prev;
+        hashTable[key].next = tail;
+        hashTable[key].prev.next = hashTable[key];
+        tail.prev = hashTable[key];
+    }
+    public void add(int key, int value)
+    {
+        hashTable[key] = new Node(key,value);
+        hashTable[key].prev = tail.prev;
+        hashTable[key].next = tail;
+        hashTable[key].prev.next = hashTable[key];
+        tail.prev = hashTable[key];
+        ++size;
+    }
+    public void updateAndBubbleUp(int key, int value)
+    {
+        hashTable[key].value = value;
+        bubbleUp(key);
+    }
+    public void removeLRU()
+    {
+        //the next node to head dummy is the least recently used node
+        int key = head.next.key;
+        hashTable[key].next.prev = head;
+        head.next = hashTable[key].next;
+        hashTable[key].prev = null;
+        hashTable[key].next = null;
+        hashTable[key] = null;
+    }
+    public int getValue(int key)
+    {
+        bubbleUp(key);
+        return hashTable[key].value;
+    }
+    public boolean isPresent(int key)
+    {
+        return hashTable[key] == null ? false : true;
+    }
+    public boolean isOverflow()
+    {
+        return size > capacity ? true : false;
+    }
+}
+
+class LRUCache {
+    Cache cache;
+    
+    public LRUCache(int capacity) {
+        cache = new Cache(capacity);
+    }
+    
+    public int get(int key) {
+        if (cache.isPresent(key))
+            return cache.getValue(key);
+        return -1;
+    }
+    
+    public void put(int key, int value) {
+        if (cache.isPresent(key))
+            cache.updateAndBubbleUp(key,value);
+        else
+        {
+            cache.add(key,value);
+            if (cache.isOverflow())
+                cache.removeLRU();
+        }
+    }
+}

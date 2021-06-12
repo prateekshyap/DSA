cf0a6e465929a896eca4e369fc491ba9530ed5ff
pratik997 <pratikparidabubu@gmail.com>
2021-06-12 16:01:44 +0530

    12/06

--- Arrays/SubarrayWithGivenSum.java
+++ Arrays/SubarrayWithGivenSum.java
@@ -1,5 +1,6 @@
 /*https://practice.geeksforgeeks.org/problems/subarray-with-given-sum-1587115621/1*/
 
+/*Prateekshya's Solution*/
 class Solution
 {
     static ArrayList<Integer> list;
@@ -30,5 +31,44 @@ class Solution
         }
         list.add(-1);
         return list;
+    }
+}
+
+/*Pratik's Solution*/
+
+class Solution
+{
+    static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
+    {
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        int sum = arr[0];
+        int left=0,right=1;
+        while(right<n)
+        {
+            while(right<n && sum<s)
+            {
+                sum+=arr[right++];
+            }
+            if(sum==s)
+            {
+                al.add(left+1);
+                al.add(right);
+                return al;
+            }
+            while(left<right && sum>s)
+            {
+                sum-=arr[left++];
+            }
+        }
+        if(sum==s)
+        {
+            al.add(left+1);
+            al.add(right);
+        }
+        else
+        {
+            al.add(-1);
+        }
+        return al;
     }
 }

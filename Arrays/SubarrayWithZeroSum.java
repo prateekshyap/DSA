90338103dea40ef0bef078a040aae098da321ca1
pratik997 <pratikparidabubu@gmail.com>
2021-06-12 16:17:19 +0530

    12/06

--- Arrays/SubarrayWithZeroSum.java
+++ Arrays/SubarrayWithZeroSum.java
@@ -1,5 +1,5 @@
 /*https://practice.geeksforgeeks.org/problems/subarray-with-0-sum-1587115621/1*/
-
+/*Prateekshya's Solution*/
 class Solution{
     //Function to check whether there is a subarray present with 0-sum or not.
     static boolean findsum(int arr[],int n)
@@ -19,6 +19,24 @@ class Solution{
             
             //add to the hashmap
             map.put(sum,i);
+        }
+        return false;
+    }
+}
+/*Pratik's Solution*/
+class Solution{
+    //Function to check whether there is a subarray present with 0-sum or not.
+    static boolean findsum(int arr[],int n)
+    {
+        //Your code here
+        HashSet<Integer> set = new HashSet<Integer>();
+        set.add(arr[0]);
+        for(int i=1;i<n;i++)
+        {
+            arr[i]+=arr[i-1];
+            if(arr[i]==0)return true;
+            if(!set.contains(arr[i]))set.add(arr[i]);
+            else return true;
         }
         return false;
     }

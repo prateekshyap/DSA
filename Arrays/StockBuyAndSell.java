4720eb516ad19f39828bc5a11f1632ee8ce887dc
pratik997 <pratikparidabubu@gmail.com>
2021-06-12 11:52:13 +0530

    12/06

--- Arrays/StockBuyAndSell.java
+++ Arrays/StockBuyAndSell.java
@@ -1,5 +1,6 @@
 /*https://practice.geeksforgeeks.org/problems/stock-buy-and-sell-1587115621/1*/
 
+/*Prateekshya's Solution*/
 import java.io.*;
 
 class GFG
@@ -52,4 +53,36 @@ class GFG
 		        System.out.println("No Profit");
 	    }
 	}
+}
+
+/*Pratik's Solution*/
+class Solution{
+    //Function to find the days of buying and selling stock for max profit.
+    ArrayList<ArrayList<Integer> > stockBuySell(int A[], int n) {
+        ArrayList<ArrayList<Integer>> aList = new ArrayList<ArrayList<Integer>>();
+        int buy = 0, sell =buy+1;
+        while(sell<n)
+        {
+            ArrayList<Integer> al = new ArrayList<Integer>();
+            while(sell<n && A[sell]<=A[buy])
+            {
+                buy=sell;
+                sell=buy+1;
+            }
+            while(sell<n-1 && A[sell]<A[sell+1])
+            {
+                sell++;
+            }
+            if(buy<n && sell<n)
+            {
+                al.add(buy);
+                al.add(sell);
+                aList.add(al);
+            }
+            buy=sell+1;
+            sell=buy+1;
+        }
+        //System.out.print(aList);
+        return aList;
+    }
 }

e06308664c004d6d4eb9a237c160ace8c76dbe02
pratik997 <pratikparidabubu@gmail.com>
2021-06-16 15:23:46 +0530

    16/06

--- Strings/StringRotationCheck.java
+++ Strings/StringRotationCheck.java
@@ -1,5 +1,7 @@
 /*https://leetcode.com/problems/rotate-string/*/
 
+/*Prateekshya's Solution*/
+
 class Solution {
     public boolean rotateString(String s, String goal) {
         if (s.length() == 0 && goal.length() == 0) return true;
@@ -13,6 +15,26 @@ class Solution {
         	//if goal is a substring of the concatenated string then return true
             if (goal.equalsIgnoreCase(concat.substring(i,i+goal.length())))
                 return true;
+        return false;
+    }
+}
+
+/*Pratik's Solution*/
+
+class Solution {
+    public boolean rotateString(String s, String goal) 
+    {
+        if(s.length()!=goal.length())return false;
+        if(s.length()==0 || s.equals(goal))return true;
+        int i = 0 , n = s.length();
+        while(i!=n)
+        {
+            if((s.substring(i,n)).equals(goal.substring(0,(n-i))))
+            {
+                if((goal.substring(n-i,n)).equals(s.substring(0,i)))return true;
+            }
+            i++;
+        }
         return false;
     }
 }

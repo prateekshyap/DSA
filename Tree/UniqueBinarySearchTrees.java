3a1d18d9ae43a9b22f0eb9a07780626af3a75bda
prateekshyap <prateekshyapriyadarshini@gmail.com>
2021-12-19 17:53:20 +0530

    19/12

--- Tree/UniqueBinarySearchTrees.java
+++ Tree/UniqueBinarySearchTrees.java
@@ -1,18 +1,83 @@
-/*https://leetcode.com/problems/unique-binary-search-trees/*/
+/*https://leetcode.com/problems/unique-binary-search-trees-ii/*/
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public int numTrees(int n) {
-        int[] table = new int[n+1];
-        table[0] = 1;
-        table[1] = 1;
+    public List<TreeNode> generateTrees(int n) {
+         return createPairs(1,n);
+    }
+    public List<TreeNode> createPairs(int start,int end)
+    {
+        List<TreeNode> result = new ArrayList<TreeNode>();
         
-        for (int i = 2; i <= n; ++i)
+        for (int i = start; i <= end; ++i)
         {
-            table[i] = 0;
-            for (int j = 0; j < i; ++j)
-               table[i] += table[j] * table[i-j-1];
-        }    
-        
-        return table[n];
+            List<TreeNode> rightPairs = null;
+            List<TreeNode> leftPairs = null;
+            if (start == end)
+            {
+                TreeNode res = new TreeNode(i);
+                result.add(res);
+                return result;
+            }
+            if (i == start)
+                rightPairs = createPairs(i+1,end);
+            if (i == end)
+                leftPairs = createPairs(start,i-1);
+            if (i != start && i != end)
+            {
+                 rightPairs = createPairs(i+1,end);
+                 leftPairs = createPairs(start,i-1);
+            }
+            
+            if (rightPairs != null && leftPairs == null)
+            {
+                for (TreeNode r : rightPairs)
+                {
+                    TreeNode res = new TreeNode(i);
+                    res.right = r;
+                    result.add(res);
+                }
+                continue;
+            }
+            if (leftPairs != null && rightPairs == null)
+            {    
+                for (TreeNode l : leftPairs)
+                {
+                    TreeNode res= new TreeNode(i);
+                    res.left=l;
+                    result.add(res);
+                }
+                continue;
+            }
+            if (leftPairs != null && rightPairs != null)
+            {
+                for (TreeNode l : leftPairs)
+                {
+                    for (TreeNode r : rightPairs)
+                    {
+                        TreeNode res = new TreeNode(i);
+                        res.left = l;
+                        res.right = r;
+                        result.add(res);
+                    }
+                }
+                continue;
+            } 
+        }
+        return result;
     }
 }

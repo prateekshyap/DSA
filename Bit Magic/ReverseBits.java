aa076a8ecae13595c17bbeaebe45ef3819a68a01
prateekshyap <prateekshyapriyadarshini@gmail.com>
2021-12-22 00:07:15 +0530

    20/12

--- Bit Magic/ReverseBits.java
+++ Bit Magic/ReverseBits.java
@@ -14,3 +14,38 @@ class Solution
         return result;
     }
 }
+
+/*https://leetcode.com/problems/reverse-bits/*/
+
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n)
+    {
+        int answer = 0;
+        int bit = 0;
+        for (int i = 0; i < 32; i++) {
+            bit = n & 1;
+            answer = (answer << 1) + bit;
+            n = n >>> 1;
+        }
+        return answer;
+    }
+    /*int power = 1;
+    public int reverseBits(int n) {
+        StringBuffer result = new StringBuffer(Integer.toBinaryString(n));
+        result.reverse();
+        while (result.length() < 32)
+            result.append("0");
+        int returnValue = getDecimalValue(result, 0);
+        return returnValue;
+    }
+    public int getDecimalValue(StringBuffer result, int index)
+    {
+        if (index == result.length()-1)
+            return Integer.parseInt(Character.toString(result.charAt(index)));
+        int decimal = getDecimalValue(result,index+1);
+        power <<= 1;
+        decimal += Integer.parseInt(Character.toString(result.charAt(index)))*power;
+        return decimal;
+    }*/
+}

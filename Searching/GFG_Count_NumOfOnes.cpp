2d6dd24424e65a9777a982c5472f2b2ea60f97b7
Vijay Purohit <vijay.pu9@gmail.com>
2022-02-07 20:49:35 +0530

    code karo abhiyan

---
+++ Searching/GFG_Count_NumOfOnes.cpp
@@ -0,0 +1,43 @@
+/*
+https://www.geeksforgeeks.org/count-1s-sorted-binary-array/
+
+*/
+/* Return counts of 1's in arr[low..high].  The array is
+   assumed to be sorted in non-increasing order */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int binary_left_right_bound(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    int i=0, j = n-1, mid=-1;
+    while(i<=j){
+        mid = i + (j-i)/2;
+        if(target>nums[mid]) j =mid-1;
+        else if (target<nums[mid]) i=mid+1;
+        else{
+            if(mid == n-1 || nums[mid+1] != target)
+                return mid+1; // as it is sorted return mid +1 (index from 0)
+            else
+                i=mid+1; // find rightmost target index
+
+        }
+    }
+}
+int countOnesInArray(vector<int> &nums, int target){
+
+    /**** Approach 1 USING Binary Search *****/
+    return binary_left_right_bound(nums, target);
+}
+
+int main()
+{
+    vector<int> arr = { 1, 1, 0, 0, 0, 0,0};
+    cout << "Count of 1's in given array is "
+         << countOnesInArray(arr, 1);
+    return 0;
+}
---
+++ Searching/LC_34_SearchRotatedSortedArray.cpp
@@ -0,0 +1,86 @@
+/*
+https://leetcode.com/problems/search-in-rotated-sorted-array/submissions/
+33. Search in Rotated Sorted Array
+*/
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+       
+        int n = nums.size();
+        
+        if(n == 0) return -1;
+        
+        // if array is already sorted without rotation
+        if(nums[0]<=nums[n-1])
+            return binary_search(nums, 0, n-1, target);
+        
+        int pivot_index = findPivotElementIndex(nums, 0, n-1);
+        
+        if(nums[pivot_index]==target) 
+            return pivot_index; // if pivot index element itself is the target
+        // else if (target > nums[pivot_index] && target <=nums[n-1])
+        //     return binary_search(nums, pivot_index+1, n-1, target); // search in right array
+        // else 
+        //     return binary_search(nums,     0,        pivot_index-1, target); // search in left array
+        
+        int i =0;
+        int j = n-1;
+        
+        if (target > nums[pivot_index] && target <=nums[n-1])
+            i = pivot_index+1;
+        else{
+            j=pivot_index-1;
+        }
+        
+         int mid = -1;
+        while(i<=j)
+        {
+            mid = i + (j-i)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid] < target)
+                i=mid+1;
+            else 
+                j=mid-1;
+        }
+        return -1;
+        
+    }// end
+    
+    int binary_search(vector<int>&nums, int i, int j, int target){
+         int mid = -1;
+        while(i<=j)
+        {
+            mid = i + (j-i)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid] < target)
+                i=mid+1;
+            else 
+                j=mid-1;
+        }
+        return -1;
+    }
+    
+    int findPivotElementIndex(vector<int>& nums, int i, int j){
+        
+        int  mid = -1;
+        
+        while(i<j)
+        {
+            mid = i + (j-i)/2;
+            // [4,5,6,7,0,1,2] // first IF find index 4, second IF index 4
+            // [0,1,2,4,5,6,7]  // IF index 6, second IF index 0
+            // if(nums[0]<=nums[mid]) 
+            if(nums[mid] > nums[j]) // finding smallest element in the array index
+                i=mid+1;
+            else
+                j=mid;
+            
+            // cout<<" i"<<i<<" ,j"<<j<<": ";
+        }
+        return i;
+        
+        
+    }//end
+};

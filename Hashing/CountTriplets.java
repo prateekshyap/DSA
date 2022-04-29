0db0e72c8ce32791e1533aa2dcc06719486f5343
prateekshyap <prateekshyapriyadarshini@gmail.com>
2022-04-29 15:12:29 +0530

    29/04

---
+++ Hashing/CountTriplets.java
@@ -0,0 +1,49 @@
+/*https://www.hackerrank.com/challenges/count-triplets-1/*/
+
+public class Solution {
+    // Complete the countTriplets function below.
+    static long countTriplets(List<Long> arr, long r) {
+        Map<Long, Long> potential = new HashMap<>();
+        Map<Long, Long> counter = new HashMap<>();
+        long count = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            long a = arr.get(i);
+            long key = a / r;
+            
+            if (counter.containsKey(key) && a % r == 0) {
+                count += counter.get(key);
+            }
+            
+            if (potential.containsKey(key) && a % r == 0) {
+                long c = potential.get(key);
+                counter.put(a, counter.getOrDefault(a, 0L) + c);
+            }
+            
+            potential.put(a, potential.getOrDefault(a, 0L) + 1); // Every number can be the start of a triplet.
+        }
+        return count;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nr = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(nr[0]);
+
+        long r = Long.parseLong(nr[1]);
+
+        List<Long> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Long::parseLong)
+            .collect(toList());
+
+        long ans = countTriplets(arr, r);
+
+        bufferedWriter.write(String.valueOf(ans));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
